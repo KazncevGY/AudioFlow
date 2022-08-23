8a3ddac6132898137c57b2b52d1023ef7d20bb07
Kazancev Grigorij <kazancev.grigorij@gmail.com>
2022-08-23 16:00:14 +0700

    Add AudioFlow project. Add initial implementation of playable

---
+++ AudioFlow/Playable/IPlayableItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioFlow.Playable
+{
+    /// <summary>
+    /// Базовый интерфейс для источника данных. Привязанные к аудиодвижкам реализации должны реализовывать его.
+    /// </summary>
+    public interface IPlayableItem
+    {
+        public int SamplesRate { get; }
+
+        public bool IsEnded { get; }
+
+        public float[] ReadFragment(int length);
+    }
+}
---
+++ AudioFlow/Playable/NA_PlayableItem.cs
@@ -0,0 +1,32 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioFlow.Playable
+{
+    /// <summary>
+    /// Обертка для реализаций ISampleProvider из NAudio.
+    /// </summary>
+    public class NA_PlayableItem : IPlayableItem
+    {
+        private ISampleProvider _source;
+
+        public int SamplesRate => _source.WaveFormat.SampleRate;
+
+        public bool IsEnded {get; private set;}
+
+        public float[] ReadFragment(int length)
+        {
+            float[] buffer=new float[length];
+            int samplesRead=_source.Read(buffer, 0, length);
+            if (samplesRead < length)
+            {
+                IsEnded = true;
+            }
+            return buffer;
+        }
+    }
+}
